0a9dd86958455108e2463c663fbea7917d4314f5
Gustavo Garcia <gst.garc@gmail.com>
2024-11-16 01:34:53 -0300

    feat: add nextButton and prevButton

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,21 @@ async function buildResponse(response: Response): Promise<void> {
 
     const data = await response.json();
     resultDiv.innerHTML = getData(data.items).innerHTML;
+
+    const buttonDiv: HTMLDivElement = document.createElement('div');
+    buttonDiv.id = 'nav-buttons';
+
+    const previousButton: HTMLButtonElement | null = getPreviousButton(response);
+    if (previousButton) {
+        buttonDiv.appendChild(previousButton);
+    }
+
+    const nextButton: HTMLButtonElement | null = getNextButton(response);
+    if (nextButton) {
+        buttonDiv.appendChild(nextButton);
+    }
+
+    resultDiv.appendChild(buttonDiv);
 }
 
 function getData(items: any[]): HTMLDivElement {
@@ -72,4 +87,72 @@ function getData(items: any[]): HTMLDivElement {
     });
 
     return resultDiv;
+}
+
+function getNextButton(response: Response): HTMLButtonElement | null {
+    const links: string | null = response.headers.get('link');
+    if (!links) {
+        return null;
+    }
+
+    let rawLink: string | undefined = links.split(',').find((link: string) => link.includes('rel="next"'));
+    if (!rawLink) {
+        return null;
+    }
+
+    const match: RegExpMatchArray | null = rawLink.match(/<(.+)>/);
+    if (!match) {
+        return null;
+    }
+
+    const nextLink: string = match[1];
+
+    const a = document.createElement('a');
+    a.href = nextLink;
+
+    const button = document.createElement('button');
+    button.textContent = 'Next';
+    button.id = 'next-button';
+    button.addEventListener('click', async (e: Event) => {
+        e.preventDefault();
+        const request: Request = buildRequest(nextLink);
+        const response: Response = await fetch(request);
+        await buildResponse(response);
+    });
+
+    return button;
+}
+
+function getPreviousButton(response: Response): HTMLButtonElement | null {
+    const links: string | null = response.headers.get('link');
+    if (!links) {
+        return null;
+    }
+
+    let rawLink: string | undefined = links.split(',').find((link: string) => link.includes('rel="prev"'));
+    if (!rawLink) {
+        return null;
+    }
+
+    const match: RegExpMatchArray | null = rawLink.match(/<(.+)>/);
+    if (!match) {
+        return null;
+    }
+
+    const prevLink: string = match[1];
+
+    const a = document.createElement('a');
+    a.href = prevLink;
+
+    const button = document.createElement('button');
+    button.textContent = 'Previous';
+    button.id = 'prev-button';
+    button.addEventListener('click', async (e: Event) => {
+        e.preventDefault();
+        const request: Request = buildRequest(prevLink);
+        const response: Response = await fetch(request);
+        await buildResponse(response);
+    });
+
+    return button;
 }
